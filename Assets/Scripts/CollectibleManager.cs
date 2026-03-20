f5d6b49baa23175958cb6bd6bb09cab28a3c1dba
RyanB565 <rblanco@mail.bradley.edu>
2026-03-19 22:01:37 -0500

    Collectibles
    Can collect the collectible, see how many you have collected and sends you to the next level once collected and go to the door

---
+++ Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+/*****************************************************************************
+ // File Name      : CollectibleManager.cs
+ // Author         : Ryan Blanco
+ // Creation Date  : March 18, 2026
+ //
+ // Brief Description : tracks collectables and spawns exit once we have all collectables collected and sends to 
+// next level
+ *****************************************************************************/
+public class CollectibleManager : MonoBehaviour
+{
+    private int totalCollectibles;
+
+    // How many collectibles the player has collected
+    private int collectedCount;
+
+    // Reference to the exit door object
+    [SerializeField] private GameObject exitDoor;
+
+    /// <summary>
+    /// Counts all collectibles in the scene at the start of the game.
+    /// </summary>
+    private void Start()
+    {
+        // Count all objects tagged as collectibles
+        totalCollectibles = GameObject.FindGameObjectsWithTag("CollectibleTag").Length;
+
+        collectedCount = 0;
+
+        // Make sure the exit starts locked (disabled)
+        if (exitDoor != null)
+        {
+            exitDoor.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Called by the collectible when the player picks it up.
+    /// </summary>
+    public void AddCollectible()
+    {
+        collectedCount++;
+
+        // If the player collected everything, unlock the exit
+        if (collectedCount >= totalCollectibles)
+        {
+            UnlockExit();
+        }
+    }
+
+    /// <summary>
+    /// Unlocks the exit door so the player can win.
+    /// </summary>
+    private void UnlockExit()
+    {
+        if (exitDoor != null)
+        {
+            exitDoor.SetActive(true);
+        }
+
+        Debug.Log("All collectibles found! Exit unlocked.");
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+    
