b978b4bd24d8ce38b970fcba30f221a8125f0f00
RyanB565 <rblanco@mail.bradley.edu>
2026-04-16 19:29:45 -0500

    Simple pause menu is in

--- Assets/Scripts/LevelInitilizer.cs
+++ Assets/Scripts/LevelInitilizer.cs
@@ -1,10 +1,27 @@
+﻿/*****************************************************************************
+ // File Name      : LevelInitializer.cs
+ // Author         : Ryan Blanco
+ // Creation Date  : April 14, 2026
+ //
+ // Brief Description :
+ // Sets the current level number when the scene loads. Used by LevelManager
+ // to enable or disable level‑specific features.
+ *****************************************************************************/
+
+
+
+
 using UnityEngine;
 
 public class LevelInitilizer : MonoBehaviour
 {
-    public int levelNumber = 2;
+    public int levelNumber = 2; //lvel number 
 
-    void Start()
+   
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Start() // assings level number to level manager
     {
         LevelManager.currentLevel = levelNumber;
     }
